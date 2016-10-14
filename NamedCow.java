d10ad5cbe2a373533248cefb5cf9106eaccf41e5
pastalover45 <kendrashu@gmail.com>
2016-10-14 11:34:28 -0700

    tinkered with classes

--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,13 @@
-class NamedCow
-{
+class NamedCow extends Cow {
+
+	String myName;
+
+	NamedCow(String type,String name, String sound){
+
+		myName = name;
+
+	}
+
+	public String getName(){return myName;}
 
 }
